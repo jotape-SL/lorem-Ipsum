e83bb14faeb3c69fa14b167adefb7712bfc6113f
João Pedro de Sousa Cruz <jolunyna@gmail.com>
2022-07-12 13:31:33 -0300

    displayed the data

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import React, { useState } from 'react';
 import data from './data';
 function App() {
   const [count, setCount] = useState(0);
-  const [text, setText] = useState('');
+  const [text, setText] = useState([]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    let amount = parseInt(count);
+    setText(data.slice(0, amount));
   };
-
   return (
     <section className='section-center'>
       <h3>tired of boring lorem ipsum?</h3>
-      <form action='' className='lorem-form' onSubmit={handleSubmit}>
+      <form className='lorem-form' onSubmit={handleSubmit}>
         <label htmlFor='amount'>paragraphs:</label>
         <input
           type='number'
@@ -20,17 +21,12 @@ function App() {
           value={count}
           onChange={(e) => setCount(e.target.value)}
         />
-        <button type='submit' className='btn'>
-          generate
-        </button>
+        <button className='btn'>generate</button>
       </form>
       <article className='lorem-text'>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, autem?
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, autem?
-        </p>
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
       </article>
     </section>
   );
